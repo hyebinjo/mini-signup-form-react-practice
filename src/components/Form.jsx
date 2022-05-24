de36404c2a35379ed3ffde6e762017e5427d216c
hyebinjo <hyebinjo91@gmail.com>
2022-05-24 17:58:18 +0900

    feat: 유효성검사 & 커스텀에러메세지

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,107 @@
 import InputForm from './InputForm'
+import { useState, createContext } from 'react'
+
+const initialFormData = {
+    id: '',
+    pw: '',
+    comfirmPw: '',
+}
+
+export const FormData = createContext({
+    formData: initialFormData,
+    setFormData: () => {},
+})
+
+const ErrorMsg = {
+    required: '필수 정보입니다.',
+    invalidId:
+        '5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.',
+    invalidPw: '8~16자 영문 대 소문자, 숫자를 사용하세요.',
+    invalidComfirmPw: '비밀번호가 일치하지 않습니다.',
+}
 
 const Form = () => {
+    const [formData, setFormData] = useState(initialFormData)
+    function checkId(value) {
+        const validId = /^([a-z]|[0-9]|_|-){5,20}$/g
+        return validId.test(value)
+    }
+
+    function checkPassword(value) {
+        const validPw = /^([a-z]|[0-9]){8,16}$/gi
+        return validPw.test(value)
+    }
+
+    function confirmPassword(value) {
+        return formData.pw === value
+    }
     return (
-        <form
-            id="form"
-            className="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            autoComplete="off"
-        >
-            <InputForm
-                id={'id'}
-                label={'아이디'}
-                inputProps={{
-                    className:
-                        'shadow border rounded w-full py-2 px-3 text-gray-700',
-                    type: 'text',
-                    placeholder: '아이디를 입력해주세요.',
-                }}
-            />
-            <InputForm
-                id={'pw'}
-                label={'비밀번호'}
-                inputProps={{
-                    className:
-                        'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight',
-                    type: 'password',
-                    placeholder: '비밀번호를 입력해주세요',
-                    autoComplete: 'off',
-                }}
-            />
-            <InputForm
-                id={'pw-check'}
-                label={'비밀번호 확인'}
-                inputProps={{
-                    className:
-                        'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight',
-                    type: 'password',
-                    placeholder: '비밀번호를 입력해주세요',
-                    autoComplete: 'off',
-                }}
-            />
-            <div className="flex items-center justify-center">
-                <input
-                    id="submit"
-                    type="submit"
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
-                    value="가입하기"
+        <FormData.Provider value={{ formData, setFormData }}>
+            <form
+                id="form"
+                className="w-full max-w-md m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                autoComplete="off"
+            >
+                <InputForm
+                    id={'id'}
+                    label={'아이디'}
+                    inputProps={{
+                        className:
+                            'shadow border rounded w-full py-2 px-3 text-gray-700',
+                        type: 'text',
+                        placeholder: '아이디를 입력해주세요.',
+                    }}
+                    checkValue={checkId}
+                    msgId={'id-msg'}
+                    errorMsg={{
+                        required: ErrorMsg.required,
+                        invalid: ErrorMsg.invalidId,
+                    }}
                 />
-            </div>
-        </form>
+                <InputForm
+                    id={'pw'}
+                    label={'비밀번호'}
+                    inputProps={{
+                        className:
+                            'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight',
+                        type: 'password',
+                        placeholder: '비밀번호를 입력해주세요',
+                        autoComplete: 'off',
+                    }}
+                    checkValue={checkPassword}
+                    msgId={'pw-msg'}
+                    errorMsg={{
+                        required: ErrorMsg.required,
+                        invalid: ErrorMsg.invalidPw,
+                    }}
+                />
+                <InputForm
+                    id={'pw-check'}
+                    label={'비밀번호 확인'}
+                    inputProps={{
+                        className:
+                            'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight',
+                        type: 'password',
+                        placeholder: '비밀번호를 입력해주세요',
+                        autoComplete: 'off',
+                    }}
+                    checkValue={confirmPassword}
+                    msgId={'pw-check-msg'}
+                    errorMsg={{
+                        required: ErrorMsg.required,
+                        invalid: ErrorMsg.invalidComfirmPw,
+                    }}
+                />
+                <div className="flex items-center justify-center">
+                    <input
+                        id="submit"
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:bg-gray-500"
+                        value="가입하기"
+                    />
+                </div>
+            </form>
+        </FormData.Provider>
     )
 }
 
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,7 +1,24 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useContext } from 'react'
+import { FormData } from './Form'
 
-const InputForm = ({ id, label, inputProps }) => {
+const InputForm = ({ id, label, inputProps, checkValue, msgId, errorMsg }) => {
     const inputRef = useRef(null)
+    const { formData, setFormData } = useContext(FormData)
+    const [error, setError] = useState()
+
+    const validate = () => {
+        if (!inputRef.current.value) {
+            inputRef.current.classList.add('border-red-600')
+            setError(errorMsg.required)
+        } else if (!checkValue(inputRef.current.value)) {
+            inputRef.current.classList.add('border-red-600')
+            setError(errorMsg.invalid)
+        } else {
+            inputRef.current.classList.remove('border-red-600')
+            setError(null)
+            setFormData((prev) => ({ ...prev, [id]: inputRef.current.value }))
+        }
+    }
     useEffect(() => {
         if (id === 'id') inputRef.current.focus()
     }, [])
@@ -13,8 +30,15 @@ const InputForm = ({ id, label, inputProps }) => {
             >
                 {label}
             </label>
-            <input id={id} {...inputProps} ref={inputRef} />
-            <div className="mt-1 mb-3 text-xs text-red-500"></div>
+            <input
+                id={id}
+                {...inputProps}
+                ref={inputRef}
+                onBlur={() => validate()}
+            />
+            <div id={msgId} className="mt-1 mb-3 text-xs text-red-500">
+                {error}
+            </div>
         </div>
     )
 }
